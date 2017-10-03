6bcf3d3439cd17cce87dfb7399ef1d2670d09ed0
mleibert <leiberml@prc-internal.prc.gov>
2017-10-03 16:28:54 -0400

    1

--- hw4.R
+++ hw4.R
@@ -14,23 +14,51 @@ C
 
 A-C
 
-A %*% t(B)
+t(B)%*%  A
+
+A%*%C
+t(C)%*%  A
+
+pmatrix(t(C)%*%  A,digits=0)
+
+
+##  5.15
+
+mat<-matrix(c(5,2,23,7),2,2,byrow=T);mat
+solve(mat,c(8,28))
 
 
 #####
+
 x<-c(4, 1, 2, 3, 3, 4)
 Y<-c(16, 5, 10, 15, 13, 22)
 X<-matrix(0,length(x),2);X[,1]<-1;X[,2]<-x;X
 Y<-as.matrix(Y)
+n=6
+solve(t(X)%*%X)%*%t(X)%*%Y
+b<-solve(t(X)%*%X)%*%t(X)%*%Y;b
+e<-Y-X%*%b;e
 
-as.matrix(X)%*%t(as.matrix(X))
-pmatrix(t(X),digits=0)
 
-pmatrix(t(Y),digits=0)
-pmatrix(Y,digits=0)
-pmatrix(Y%*%t(Y),digits=0)
+J<-matrix(rep(1,6^2),6,6)
+t(b)%*%t(X)%*%Y - ((1/n)*t(Y))%*%J%*%Y
+t(Y) %*% Y - t(b) %*% t(X) %*% Y
 
-pmatrix(t(X),digits=0)
-pmatrix(Y,digits=0)
-pmatrix(t(X)%*%Y,digits=0)
+var(t(X)%*%X)
+MSE<-sum(e^2)/(n-2)
+denom<-sum((x-mean(x))^2);var(x)*(n-1)
+
+matrix(c((MSE/n) + ( (mean(x)^2 * MSE) / denom),
+rep((-mean(x)*MSE)/denom,2), (MSE)/denom),2,2,byrow=T)
+
+ solve(t(X)%*%X
+
+Xh<-matrix(c(1,4),2,1)
+MSE*(1+t(Xh)%*% solve(t(X)%*%X) %*%Xh)
+
+
+H<-X%*%solve(t(X)%*%X)%*%t(X);H
+H%*%H
+
+MSE*()
 
